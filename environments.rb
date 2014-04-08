aef2b71ea9117ee1712f8b84f51425c1a2c237c4
Simon <simon.slater@gmail.com>
2014-04-08 18:08:02 -0400

    harp

--- environments.rb
+++ environments.rb
@@ -4,8 +4,16 @@ configure :development do
 end
 
 configure :production do
-ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres:localhost/mydb')
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
 
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
 end
 
 def streaming_twitter
